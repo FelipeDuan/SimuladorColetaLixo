b797764bed57b6ae116ab2c0ff3e8f1e1861e603
adryanrr <adryanrrdev@gmail.com>
2025-04-02 22:20:32 -0300

    Spring Initializr

---
+++ src/main/java/edu/icev/TeresinaCollectionSim/TeresinaCollectionSimApplication.java
@@ -0,0 +1,13 @@
+package edu.icev.TeresinaCollectionSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeresinaCollectionSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeresinaCollectionSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/icev/TeresinaCollectionSim/TeresinaCollectionSimApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icev.TeresinaCollectionSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeresinaCollectionSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
