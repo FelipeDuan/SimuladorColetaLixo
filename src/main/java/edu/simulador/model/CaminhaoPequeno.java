f73aee02f572b2514ace033631ad55c9f01cb92e
FelipeDuan <felipeduan1@gmail.com>
2025-04-05 23:20:48 -0300

    feat: teste

--- src/main/java/edu/simulador/model/CaminhaoPequeno.java
+++ src/main/java/edu/simulador/model/CaminhaoPequeno.java
@@ -1,4 +1,38 @@
 package edu.simulador.model;
 
 public class CaminhaoPequeno {
+    String id;
+    float capacidadeMaxima;
+    float cargaAtual;
+    int numeroDeViagensDiarias;
+    String zonaAtual;
+    boolean emViagem;
+    float tempoCarregamentoMin;
+
+    public CaminhaoPequeno(String id, float capacidadeMaxima, float cargaAtual, int numeroDeViagensDiarias, String zonaAtual, boolean emViagem, float tempoCarregamentoMin) {
+//        this.id = id;
+//        this.capacidadeMaxima = capacidadeMaxima;
+//        this.cargaAtual = cargaAtual;
+//        this.numeroDeViagensDiarias = numeroDeViagensDiarias;
+//        this.zonaAtual = zonaAtual;
+//        this.emViagem = emViagem;
+//        this.tempoCarregamentoMin = tempoCarregamentoMin;
+    }
+
+    public void coletarLixo(int quantidade) {
+
+    }
+
+    public void despejarEstacao() {
+
+    }
+
+    public void viajarPara() {
+
+    }
+
+    public void tempoCarregar() {
+
+    }
+
 }
--- src/main/java/edu/simulador/model/Simulador.java
+++ src/main/java/edu/simulador/model/Simulador.java
@@ -1,4 +1,38 @@
 package edu.simulador.model;
 
 public class Simulador {
+    CaminhaoPequeno[] caminhoesPequenos;
+    CaminhaoGrande[] caminhoesGrandes;
+    EstacaoDeTransferencia[] estacoes;
+    float tempoSimulacao;
+    boolean rodando;
+
+    public Simulador(float tempoSimulacao) {
+        this.tempoSimulacao = tempoSimulacao;
+        this.rodando = false;
+    }
+
+    public void iniciarSimulacao() {
+        rodando = true;
+        while (rodando) {
+            System.out.println("Simulação Iniciada");
+            System.out.println("Rodando...");
+        }
+    }
+
+    public void pausar() {
+
+    }
+
+    public void continuar() {
+
+    }
+
+    public void interromper() {
+
+    }
+
+    public void gerarEstatisticas() {
+
+    }
 }
