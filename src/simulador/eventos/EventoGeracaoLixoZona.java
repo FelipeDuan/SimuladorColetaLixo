51c148522056b84023494a57314b9260c9849900
FelipeDuan <felipeduan1@gmail.com>
2025-05-18 17:48:16 -0300

    doc: EventoGeracaoLixoZona

--- src/simulador/eventos/EventoGeracaoLixoZona.java
+++ src/simulador/eventos/EventoGeracaoLixoZona.java
@@ -13,11 +13,22 @@ import simulador.zona.Zona;
 public class EventoGeracaoLixoZona extends Evento {
     private Zona zona;
 
+    /**
+     * Construtor do evento de geração de lixo em uma zona.
+     *
+     * @param tempo o tempo (em minutos) em que o evento será executado
+     * @param zona  a zona onde o lixo será gerado
+     */
     public EventoGeracaoLixoZona(int tempo, Zona zona) {
         super(tempo);
         this.zona = zona;
     }
 
+    /**
+     * Executa a geração de lixo na zona associada.
+     * <p>
+     * A quantidade gerada é definida aleatoriamente conforme os limites da zona.
+     */
     @Override
     public void executar() {
         zona.gerarLixoDiario();
